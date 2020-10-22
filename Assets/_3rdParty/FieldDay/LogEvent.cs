9caddeaeff516c4c5397b8eab9a751a6e8c74087
nspevacek <nspevacek@wisc.edu>
2020-10-22 14:07:18 -0500

    Add LogEvent class, remove placeholder Logger class, add click event stubs

---
+++ Assets/_3rdParty/FieldDay/LogEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace FieldDay 
+{
+    public class LogEvent : ILogEvent
+    {
+        public Dictionary<string, string> Data { get; set; }
+
+        public LogEvent(Dictionary<string, string> data)
+        {
+            Data = data;
+        }
+    }
+}
--- Assets/_Prototyping/_Shared/_Code/Services/Analytics/AnalyticsService.cs
+++ Assets/_Prototyping/_Shared/_Code/Services/Analytics/AnalyticsService.cs
@@ -18,10 +18,11 @@ namespace ProtoAqua
         [SerializeField] private string m_AppId = "Aqualab";
         [SerializeField] private int m_AppVersion = 1;
         [SerializeField] private QueryParams m_QueryParams = null;
-        
+
         #endregion // Inspector
 
         private SimpleLog m_Logger;
+        public enum ClickType { ARG, EXP, OBS, MDL };
 
         #region IService
 
@@ -43,5 +44,46 @@ namespace ProtoAqua
         }
 
         #endregion // IService
+
+        #region Logging
+
+        // TODO: The idea here is to take in the necessary log values for each type of click event,
+        //       construct a dictionary for all the values (so that the values can be passed in line
+        //       to this service), and then send the data to the logger as a LogEvent.
+
+        // Note: Used params array here for brevity, otherwise method headers could look really long.
+        //       Could be changed to list each parameter if that makes more sense / provides more clarity.
+        public void LogArgumentClick(params string[] args)
+        {
+            // Make sure that all the expected values for logging are passed in.
+            if (args.Length != 1) 
+            {
+                throw new ArgumentException("Invalid args length.");
+            }
+
+            Dictionary<string, string> data = new Dictionary<string, string>()
+            {
+                { "argumentId", args[0] }
+            };
+
+            m_Logger.Log(new LogEvent(data));
+        }
+
+        public void LogExperimentClick(params string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogObservationClick(params string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogModelingClick(params string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion // Logging
     }
 }
